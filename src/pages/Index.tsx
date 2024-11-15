834a18febe720df39f5ca53f28cc0aa164cfb141
Akash Sahu <akashsahu6691@gmail.com>
2024-11-15 15:31:08 +0530

    update path

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
 import Dishes from "./second-page/Dishes";
 import Home from "./first-page/Home";
 import Chef from "./third-page/Chef";
-import Testimonial from "./testimonial-page/testimonial";
-import Footer from "./footer-page/footer";
+import Testimonial from "./testimonial-page/Testimonial";
+import Footer from "./footer-page/Footer";
 
 function Index() {
     return (
